e5244a45b9cb625737ed5591289f313f85e82c1d
= <=>
2023-12-15 14:50:45 -0500

    Set the base path to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/coc-dashboard/',
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
