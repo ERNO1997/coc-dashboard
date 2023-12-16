8b8e00faa9bdda957d03cf2d978f85a77ca6c4d6
= <=>
2023-12-16 15:28:47 -0500

    Add progress bar and th picker

---
+++ src/components/GLoadingProgressCircular.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex justify-center align-center py-6">
+    <v-progress-circular indeterminate size="24" width="3" class="mr-4" />
+    <span>{{ message }}...</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  message: { type: String, default: 'Loading' },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/modules/members/MemberDetails.vue
+++ src/modules/members/MemberDetails.vue
@@ -3,24 +3,45 @@
     <template #actions>
       <g-primary-btn prepend-icon="mdi-check" @click="saveMember">Save</g-primary-btn>
     </template>
-    <v-form>
-      <v-text-field v-model="member.id" label="ID" />
-      <v-text-field v-model="member.name" label="Name" />
-      <v-text-field v-model="member.th" type="integer" label="TH" />
-      <v-autocomplete v-model="member.role" label="Role" :items="memberRoles" />
-    </v-form>
+    <v-card class="pb-4 px-4">
+      <g-loading-progress-circular v-if="loadingMemberState === LoadingDataState.LOADING" message="Loading member"/>
+      <v-form v-else-if="loadingMemberState === LoadingDataState.OK" class="pt-8">
+        <v-row>
+          <v-col cols="4">
+            <v-text-field v-model="member.id" label="ID" hide-details />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="member.name" label="Name" hide-details />
+          </v-col>
+          <v-col cols="4">
+            <v-autocomplete v-model="member.role" label="Role" :items="memberRoles" hide-details />
+          </v-col>
+          <v-col cols="12">
+            <g-th-picker v-model="member.th" />
+          </v-col>
+        </v-row>
+      </v-form>
+      <div v-else>
+        error
+      </div>
+    </v-card>
   </page-view>
 </template>
 
 <script setup lang="ts">
 import PageView from '@/components/PageView.vue';
-import { computed, onMounted, ref } from 'vue';
+import {computed, onMounted, ref} from 'vue';
 import type ClanMember from '@/models/clan-member';
 import GPrimaryBtn from '@/components/GPrimaryBtn.vue';
-import { ClanMemberRole } from '@/models/clan-member-role';
-import { useRoute, useRouter } from 'vue-router';
+import {ClanMemberRole} from '@/models/clan-member-role';
+import {useRoute, useRouter} from 'vue-router';
 import RouteNames from '@/router/route-names';
-import { useMembersAPI } from '@/firebase-api-db/members';
+import {useMembersAPI} from '@/firebase-api-db/members';
+import {LoadingDataState} from "@/models/enum/loading-data-state";
+import GLoadingProgressCircular from "@/components/GLoadingProgressCircular.vue";
+import GThPicker from "@/components/GThPicker.vue";
+
+const loadingMemberState = ref(LoadingDataState.LOADING);
 
 const router = useRouter();
 const route = useRoute();
@@ -29,7 +50,7 @@ const membersApi = useMembersAPI();
 const isEdit = computed(() => {
   return route.name === RouteNames.EDIT_MEMBER;
 });
-const memberId = route.params.id?.[0] ?? '';
+const memberId = route.params.id ?? '';
 
 const member = ref<ClanMember>({
   id: '',
@@ -61,8 +82,11 @@ function saveMember() {
 
 async function fetch() {
   try {
+    loadingMemberState.value = LoadingDataState.LOADING;
     member.value = await membersApi.getMember(memberId);
+    loadingMemberState.value = LoadingDataState.OK;
   } catch (error) {
+    loadingMemberState.value = LoadingDataState.ERROR;
     console.error(error);
   }
 }
